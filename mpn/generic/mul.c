c7e4b036c113e25230deb23aa1944f6ed47981ae
Torbjorn Granlund <tege@gmplib.org>
2009-06-03 13:08:08 +0200

    Major toom multiply changes.  See ChangeLog for details.

---
+++ mpn/generic/mul.c
@@ -0,0 +1,292 @@
+/* mpn_mul -- Multiply two natural numbers.
+
+   Contributed to the GNU project by Torbjorn Granlund.
+
+Copyright 1991, 1993, 1994, 1996, 1997, 1999, 2000, 2001, 2002, 2003, 2005,
+2006, 2007, 2009 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
+
+#include "gmp.h"
+#include "gmp-impl.h"
+
+
+#ifndef MUL_BASECASE_MAX_UN
+#define MUL_BASECASE_MAX_UN 500
+#endif
+
+#define TOOM33_OK(an,bn) (6 + 2 * an < 3 * bn)
+#define TOOM44_OK(an,bn) (12 + 3 * an < 4 * bn)
+
+/* Multiply the natural numbers u (pointed to by UP, with UN limbs) and v
+   (pointed to by VP, with VN limbs), and store the result at PRODP.  The
+   result is UN + VN limbs.  Return the most significant limb of the result.
+
+   NOTE: The space pointed to by PRODP is overwritten before finished with U
+   and V, so overlap is an error.
+
+   Argument constraints:
+   1. UN >= VN.
+   2. PRODP != UP and PRODP != VP, i.e. the destination must be distinct from
+      the multiplier and the multiplicand.  */
+
+mp_limb_t
+mpn_mul (mp_ptr prodp,
+	 mp_srcptr up, mp_size_t un,
+	 mp_srcptr vp, mp_size_t vn)
+{
+  ASSERT (un >= vn);
+  ASSERT (vn >= 1);
+  ASSERT (! MPN_OVERLAP_P (prodp, un+vn, up, un));
+  ASSERT (! MPN_OVERLAP_P (prodp, un+vn, vp, vn));
+
+  if (up == vp && un == vn)
+    {
+      mpn_sqr_n (prodp, up, un);
+    }
+  else if (vn < MUL_KARATSUBA_THRESHOLD)
+    { /* plain schoolbook multiplication */
+
+      /* Unless un is very large, or else if have an applicable mpn_mul_N,
+	 perform basecase multiply directly.  */
+      if (un <= MUL_BASECASE_MAX_UN
+#if HAVE_NATIVE_mpn_mul_2
+	  || vn <= 2
+#else
+	  || vn == 1
+#endif
+	  )
+	mpn_mul_basecase (prodp, up, un, vp, vn);
+      else
+	{
+	  /* We have un >> MUL_BASECASE_MAX_UN > vn.  For better memory
+	     locality, split up[] into MUL_BASECASE_MAX_UN pieces and multiply
+	     these pieces with the vp[] operand.  After each such partial
+	     multiplication (but the last) we copy the most significant vn
+	     limbs into a temporary buffer since that part would otherwise be
+	     overwritten by the next multiplication.  After the next
+	     multiplication, we add it back.  This illustrates the situation:
+
+                                                    -->vn<--
+                                                      |  |<------- un ------->|
+                                                         _____________________|
+                                                        X                    /|
+                                                      /XX__________________/  |
+                                    _____________________                     |
+                                   X                    /                     |
+                                 /XX__________________/                       |
+               _____________________                                          |
+              /                    /                                          |
+            /____________________/                                            |
+	    ==================================================================
+
+	    The parts marked with X are the parts whose sums are copied into
+	    the temporary buffer.  */
+
+	  mp_limb_t tp[MUL_KARATSUBA_THRESHOLD_LIMIT];
+	  mp_limb_t cy;
+          ASSERT (MUL_KARATSUBA_THRESHOLD <= MUL_KARATSUBA_THRESHOLD_LIMIT);
+
+	  mpn_mul_basecase (prodp, up, MUL_BASECASE_MAX_UN, vp, vn);
+	  prodp += MUL_BASECASE_MAX_UN;
+	  MPN_COPY (tp, prodp, vn);		/* preserve high triangle */
+	  up += MUL_BASECASE_MAX_UN;
+	  un -= MUL_BASECASE_MAX_UN;
+	  while (un > MUL_BASECASE_MAX_UN)
+	    {
+	      mpn_mul_basecase (prodp, up, MUL_BASECASE_MAX_UN, vp, vn);
+	      cy = mpn_add_n (prodp, prodp, tp, vn); /* add back preserved triangle */
+	      mpn_incr_u (prodp + vn, cy);
+	      prodp += MUL_BASECASE_MAX_UN;
+	      MPN_COPY (tp, prodp, vn);		/* preserve high triangle */
+	      up += MUL_BASECASE_MAX_UN;
+	      un -= MUL_BASECASE_MAX_UN;
+	    }
+	  if (un > vn)
+	    {
+	      mpn_mul_basecase (prodp, up, un, vp, vn);
+	    }
+	  else
+	    {
+	      ASSERT_ALWAYS (un > 0);
+	      mpn_mul_basecase (prodp, vp, vn, up, un);
+	    }
+	  cy = mpn_add_n (prodp, prodp, tp, vn); /* add back preserved triangle */
+	  mpn_incr_u (prodp + vn, cy);
+	}
+    }
+  else if (BELOW_THRESHOLD (vn, MUL_TOOM33_THRESHOLD) ||
+	   /* Also do larger unbalanced here, up to a (somewhat arbitrarily)
+	      larger vn limit, unless toom33 can do this product directly.  */
+	   (!TOOM33_OK (un, vn) && BELOW_THRESHOLD (vn, MUL_TOOM33_THRESHOLD * 3 / 2)))
+    {
+      /* Loop over toom42, then choose toom42, toom32, or toom22 */
+      mp_ptr ws;
+      mp_ptr scratch;
+      TMP_DECL; TMP_MARK;
+
+  #define WSALL (4 * vn)
+      ws = TMP_SALLOC_LIMBS (WSALL + 1);
+
+  #define ITCH ((un + vn) * 4 + 100)
+      scratch = TMP_SALLOC_LIMBS (ITCH + 1);
+
+      if (un >= 3 * vn)
+	{
+	  mp_limb_t cy;
+
+	  mpn_toom42_mul (prodp, up, 2 * vn, vp, vn, scratch);
+	  un -= 2 * vn;
+	  up += 2 * vn;
+	  prodp += 2 * vn;
+
+	  while (un >= 3 * vn)
+	    {
+	      mpn_toom42_mul (ws, up, 2 * vn, vp, vn, scratch);
+	      un -= 2 * vn;
+	      up += 2 * vn;
+	      cy = mpn_add_n (prodp, prodp, ws, vn);
+	      MPN_COPY (prodp + vn, ws + vn, 2 * vn);
+	      mpn_incr_u (prodp + vn, cy);
+	      prodp += 2 * vn;
+	    }
+
+	  /* FIXME: Test these in opposite order, following the philosophy of
+	     minimizing the relative overhead.  */
+	  if (5 * un > 9 * vn)
+	    {
+	      mpn_toom42_mul (ws, up, un, vp, vn, scratch);
+	      cy = mpn_add_n (prodp, prodp, ws, vn);
+	      MPN_COPY (prodp + vn, ws + vn, un);
+	      mpn_incr_u (prodp + vn, cy);
+	    }
+	  else if (9 * un > 10 * vn)
+	    {
+	      mpn_toom32_mul (ws, up, un, vp, vn, scratch);
+	      cy = mpn_add_n (prodp, prodp, ws, vn);
+	      MPN_COPY (prodp + vn, ws + vn, un);
+	      mpn_incr_u (prodp + vn, cy);
+	    }
+	  else
+	    {
+	      mpn_toom22_mul (ws, up, un, vp, vn, scratch);
+	      cy = mpn_add_n (prodp, prodp, ws, vn);
+	      MPN_COPY (prodp + vn, ws + vn, un);
+	      mpn_incr_u (prodp + vn, cy);
+	    }
+	}
+      else
+	{
+	  /* FIXME: Test these in opposite order, following the philosophy of
+	     minimizing the relative overhead.  */
+	  if (5 * un > 9 * vn)
+	    mpn_toom42_mul (prodp, up, un, vp, vn, scratch);
+	  else if (9 * un > 10 * vn)
+	    mpn_toom32_mul (prodp, up, un, vp, vn, scratch);
+	  else
+	    mpn_toom22_mul (prodp, up, un, vp, vn, scratch);
+	}
+      TMP_FREE;
+    }
+  else if (BELOW_THRESHOLD (vn, MUL_TOOM44_THRESHOLD))
+    {
+      TMP_DECL; TMP_MARK;
+      if (TOOM33_OK (un, vn))
+	{
+	  /* Apply toom33 directly, since operands are balanced enough.  */
+	  mp_ptr scratch;
+	  scratch = TMP_SALLOC_LIMBS (mpn_toom33_mul_itch (un, vn));
+	  mpn_toom33_mul (prodp, up, un, vp, vn, scratch);
+	}
+      else
+	{
+	  /* Apply toom33, recurse.  FUTURE: toom63, toom53, toom43, toom33 */
+	  mp_ptr scratch, pp;		/* FIXME: use same area for these */
+	  mp_limb_t cy;
+	  scratch = TMP_SALLOC_LIMBS (mpn_toom33_mul_itch (vn, vn));
+	  mpn_toom33_mul (prodp, up, vn, vp, vn, scratch);
+	  prodp += vn;
+	  up += vn;
+	  un -= vn;
+	  pp = TMP_SALLOC_LIMBS (2 * vn);
+	  while (un >= vn)
+	    {
+	      mpn_toom33_mul (pp, up, vn, vp, vn, scratch);
+	      cy = mpn_add_n (prodp, prodp, pp, vn);
+	      MPN_COPY (prodp + vn, pp + vn, vn);
+	      mpn_incr_u (prodp + vn, cy);
+	      prodp += vn;
+	      up += vn;
+	      un -= vn;
+	    }
+	  if (un != 0)
+	    {
+	      mpn_mul (pp, vp, vn, up, un);
+	      cy = mpn_add_n (prodp, prodp, pp, vn);
+	      MPN_COPY (prodp + vn, pp + vn, un);
+	      mpn_incr_u (prodp + vn, cy);
+	    }
+	}
+      TMP_FREE;
+    }
+  else if (BELOW_THRESHOLD ((un + vn) >> 1, MUL_FFT_THRESHOLD) ||
+	   BELOW_THRESHOLD (vn, MUL_FFT_THRESHOLD / 3)) /* FIXME */
+    {
+      TMP_DECL; TMP_MARK;
+      if (TOOM44_OK (un, vn))
+	{
+	  /* Apply toom44 directly, since operands are balanced enough.  */
+	  mp_ptr scratch;
+	  scratch = TMP_ALLOC_LIMBS (mpn_toom44_mul_itch (un, vn));
+	  mpn_toom44_mul (prodp, up, un, vp, vn, scratch);
+	}
+      else
+	{
+	  /* Apply toom44, recurse.  FUTURE: toom84, toom74, toom64, toom54, toom44 */
+	  mp_ptr scratch, pp;		/* FIXME: use same area for these */
+	  mp_limb_t cy;
+	  scratch = TMP_ALLOC_LIMBS (mpn_toom44_mul_itch (vn, vn));
+	  mpn_toom44_mul (prodp, up, vn, vp, vn, scratch);
+	  prodp += vn;
+	  up += vn;
+	  un -= vn;
+	  pp = TMP_SALLOC_LIMBS (2 * vn);
+	  while (un >= vn)
+	    {
+	      mpn_toom44_mul (pp, up, vn, vp, vn, scratch);
+	      cy = mpn_add_n (prodp, prodp, pp, vn);
+	      MPN_COPY (prodp + vn, pp + vn, vn);
+	      mpn_incr_u (prodp + vn, cy);
+	      prodp += vn;
+	      up += vn;
+	      un -= vn;
+	    }
+	  if (un != 0)
+	    {
+	      mpn_mul (pp, vp, vn, up, un);
+	      cy = mpn_add_n (prodp, prodp, pp, vn);
+	      MPN_COPY (prodp + vn, pp + vn, un);
+	      mpn_incr_u (prodp + vn, cy);
+	    }
+	}
+      TMP_FREE;
+    }
+  else
+    {
+      mpn_mul_fft_full (prodp, up, un, vp, vn);
+    }
+  return prodp[un + vn - 1];
+}
---
+++ mpn/generic/toom32_mul.c
@@ -0,0 +1,291 @@
+/* mpn_toom32_mul -- Multiply {ap,an} and {bp,bn} where an is nominally 1.5
+   times as large as bn.  Or more accurately, bn < an < 3bn.
+
+   Contributed to the GNU project by Torbjorn Granlund.
+   Improvements by Marco Bodrato.
+
+   The idea of applying toom to unbalanced multiplication is due to Marco
+   Bodrato and Alberto Zanoni.
+
+   THE FUNCTION IN THIS FILE IS INTERNAL WITH A MUTABLE INTERFACE.  IT IS ONLY
+   SAFE TO REACH IT THROUGH DOCUMENTED INTERFACES.  IN FACT, IT IS ALMOST
+   GUARANTEED THAT IT WILL CHANGE OR DISAPPEAR IN A FUTURE GNU MP RELEASE.
+
+Copyright 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
+
+
+#include "gmp.h"
+#include "gmp-impl.h"
+
+/* Evaluate in: -1, 0, +1, +inf
+
+  <-s-><--n--><--n-->
+   ___ ______ ______
+  |a2_|___a1_|___a0_|
+	|_b1_|___b0_|
+	<-t--><--n-->
+
+  v0  =  a0         * b0      #   A(0)*B(0)
+  v1  = (a0+ a1+ a2)*(b0+ b1) #   A(1)*B(1)      ah  <= 2  bh <= 1
+  vm1 = (a0- a1+ a2)*(b0- b1) #  A(-1)*B(-1)    |ah| <= 1  bh = 0
+  vinf=          a2 *     b1  # A(inf)*B(inf)
+*/
+
+#if TUNE_PROGRAM_BUILD
+#define MAYBE_mul_toom22   1
+#else
+#define MAYBE_mul_toom22						\
+  (MUL_TOOM33_THRESHOLD >= 2 * MUL_TOOM22_THRESHOLD)
+#endif
+
+#define TOOM22_MUL_N_REC(p, a, b, n, ws)				\
+  do {									\
+    if (! MAYBE_mul_toom22						\
+	|| BELOW_THRESHOLD (n, MUL_KARATSUBA_THRESHOLD))		\
+      mpn_mul_basecase (p, a, n, b, n);					\
+    else								\
+      mpn_toom22_mul (p, a, n, b, n, ws);				\
+  } while (0)
+
+void
+mpn_toom32_mul (mp_ptr pp,
+		mp_srcptr ap, mp_size_t an,
+		mp_srcptr bp, mp_size_t bn,
+		mp_ptr scratch)
+{
+  mp_size_t n, s, t;
+  int vm1_neg;
+  mp_limb_t cy;
+
+#define a0  ap
+#define a1  (ap + n)
+#define a2  (ap + 2 * n)
+#define b0  bp
+#define b1  (bp + n)
+
+  n = 1 + (2 * an >= 3 * bn ? (an - 1) / (size_t) 3 : (bn - 1) >> 1);
+
+  s = an - 2 * n;
+  t = bn - n;
+
+  ASSERT (0 < s && s <= n);
+  ASSERT (0 < t && t <= n);
+
+#define as1   (pp + n + 1)			/* n+1 */
+#define asm1  (scratch + n)			/* n+1 */
+#define bs1   (pp)				/* n+1 */
+#define bsm1  (scratch)			/* n */
+#define a0_a2 (scratch)				/* n */
+
+  /* Compute as1 and asm1.  */
+  asm1[n] = mpn_add (a0_a2, a0, n, a2, s);
+#if HAVE_NATIVE_mpn_addsub_n
+  if (asm1[n] == 0 && mpn_cmp (a0_a2, a1, n) < 0)
+    {
+      cy = mpn_addsub_n (as1, asm1, a1, a0_a2, n);
+      as1[n] = cy >> 1;
+      vm1_neg = 1;
+    }
+  else
+    {
+      cy = mpn_addsub_n (as1, asm1, a0_a2, a1, n);
+      as1[n] = asm1[n] + (cy >> 1);
+      asm1[n]-= cy & 1;
+      vm1_neg = 0;
+    }
+#else
+  as1[n] = asm1[n] + mpn_add_n (as1, a0_a2, a1, n);
+  if (asm1[n] == 0 && mpn_cmp (a0_a2, a1, n) < 0)
+    {
+      mpn_sub_n (asm1, a1, a0_a2, n);
+      vm1_neg = 1;
+    }
+  else
+    {
+      cy = mpn_sub_n (asm1, a0_a2, a1, n);
+      asm1[n]-= cy;
+      vm1_neg = 0;
+    }
+#endif
+
+  /* Compute bs1 and bsm1.  */
+  if (t == n)
+    {
+#if HAVE_NATIVE_mpn_addsub_n
+      if (mpn_cmp (b0, b1, n) < 0)
+	{
+	  cy = mpn_addsub_n (bs1, bsm1, b1, b0, n);
+	  vm1_neg ^= 1;
+	}
+      else
+	{
+	  cy = mpn_addsub_n (bs1, bsm1, b0, b1, n);
+	}
+      bs1[n] = cy >> 1;
+#else
+      bs1[n] = mpn_add_n (bs1, b0, b1, n);
+
+      if (mpn_cmp (b0, b1, n) < 0)
+	{
+	  mpn_sub_n (bsm1, b1, b0, n);
+	  vm1_neg ^= 1;
+	}
+      else
+	{
+	  mpn_sub_n (bsm1, b0, b1, n);
+	}
+#endif
+    }
+  else
+    {
+      bs1[n] = mpn_add (bs1, b0, n, b1, t);
+
+      if (mpn_zero_p (b0 + t, n - t) && mpn_cmp (b0, b1, t) < 0)
+	{
+	  mpn_sub_n (bsm1, b1, b0, t);
+	  MPN_ZERO (bsm1 + t, n - t);
+	  vm1_neg ^= 1;
+	}
+      else
+	{
+	  mpn_sub (bsm1, b0, n, b1, t);
+	}
+    }
+
+  ASSERT (as1[n] <= 2);
+  ASSERT (bs1[n] <= 1);
+  ASSERT (asm1[n] <= 1);
+/*ASSERT (bsm1[n] == 0); */
+
+#define v0    pp				/* 2n */
+#define v1    (scratch)				/* 2n+1 */
+#define vinf  (pp + 3 * n)			/* s+t */
+#define vm1   (scratch + 2 * n + 1)		/* 2n+1 */
+#define scratch_out	scratch + 4 * n + 2
+
+  /* vm1, 2n+1 limbs */
+  TOOM22_MUL_N_REC (vm1, asm1, bsm1, n, scratch_out);
+  cy = 0;
+  if (asm1[n] != 0)
+    cy = mpn_add_n (vm1 + n, vm1 + n, bsm1, n);
+  vm1[2 * n] = cy;
+
+  /* v1, 2n+1 limbs */
+  TOOM22_MUL_N_REC (v1, as1, bs1, n, scratch_out);
+  if (as1[n] == 1)
+    {
+      cy = bs1[n] + mpn_add_n (v1 + n, v1 + n, bs1, n);
+    }
+  else if (as1[n] == 2)
+    {
+#if HAVE_NATIVE_mpn_addlsh1_n
+      cy = 2 * bs1[n] + mpn_addlsh1_n (v1 + n, v1 + n, bs1, n);
+#else
+      cy = 2 * bs1[n] + mpn_addmul_1 (v1 + n, bs1, n, CNST_LIMB(2));
+#endif
+    }
+  else
+    cy = 0;
+  if (bs1[n] != 0)
+    cy += mpn_add_n (v1 + n, v1 + n, as1, n);
+  v1[2 * n] = cy;
+
+  /* vinf, s+t limbs */
+  if (s > t)  mpn_mul (vinf, a2, s, b1, t);
+  else        mpn_mul (vinf, b1, t, a2, s);
+
+  mpn_mul_n (v0, ap, bp, n);                    /* v0, 2n limbs */
+
+  /* Interpolate */
+
+  if (vm1_neg)
+    {
+#if HAVE_NATIVE_mpn_rsh1add_n
+      mpn_rsh1add_n (vm1, v1, vm1, 2 * n + 1);
+#else
+      mpn_add_n (vm1, v1, vm1, 2 * n + 1);
+      mpn_rshift (vm1, vm1, 2 * n + 1, 1);
+#endif
+    }
+  else
+    {
+#if HAVE_NATIVE_mpn_rsh1sub_n
+      mpn_rsh1sub_n (vm1, v1, vm1, 2 * n + 1);
+#else
+      mpn_sub_n (vm1, v1, vm1, 2 * n + 1);
+      mpn_rshift (vm1, vm1, 2 * n + 1, 1);
+#endif
+    }
+
+  t += s;   /* limbs in vinf */
+  if (LIKELY(t>=n)) /* We should consider this branch only, even better t>n */
+    s = n;  /* limbs in Lvinf */
+  else {
+    s = t;  /* limbs in Lvinf */
+    v1[2 * n]=0;
+  }
+
+  mpn_sub_n (v1, v1, vm1, s + n + 1);
+
+  /*
+    pp[] prior to operations:
+     |_H vinf|_L vinf|_______|_H v0__|_L v0__|
+
+    summation scheme for remaining operations:
+     |______4|n_____3|n_____2|n______|n______|pp
+     |_Hvinf_|_L*vinf|       |_H*v0__|_L v0__|
+		    || H vm1 | L vm1 |
+		     |-H vinf|-L*vinf|
+	    || H v1  | L v1  |
+	     |-H*v0  |-L v0  |
+
+    We will avoid double computation of Hv0-Lvinf. Be careful! This
+    operation can give a negative result!
+  */
+
+  cy = mpn_sub_n (v0 + n, v0 + n, vinf, s); /* Hv0-Lvinf*/
+  v1[ n + s ] += cy - mpn_sub_n(pp + 2 * n, v1, v0, n + s); /* v1-v0+Lvinf */
+  cy -= mpn_add_n (pp + n, pp + n, vm1, s); /* (Hv0-Lvinf)+Lvm1 */
+  if (cy != 0) { /* carry and borrow did not cancel one another, apply the right one */
+    if (cy == 1)
+      MPN_DECR_U (vm1 + s, 2 * n + 1 - s, 1);
+    else /* (cy == -1) */
+      MPN_INCR_U (vm1 + s, 2 * n + 1 - s, 1);
+  }
+  if (LIKELY(t>n)) { /* It works also without this "if", t<=n implies t-s==0,v1[2*n]==0 */
+    mpn_sub (vm1 + n, vm1 + n, n + 1, vinf + n, t - s); /* Hvm1-Hvinf */
+    MPN_INCR_U (vinf + s, t - s, v1[2 * n]);
+  }
+  cy = mpn_add_n (pp + n + s, pp + n + s, vm1 + s, 2 * n + 1 - s);
+  MPN_INCR_U (vinf + 1, t - 1, cy);
+
+#undef a0
+#undef a1
+#undef a2
+#undef b0
+#undef b1
+#undef v0
+#undef v1
+#undef vinf
+#undef vm1
+#undef a0_a2
+#undef bsm1
+#undef asm1
+#undef bs1
+#undef as1
+}
