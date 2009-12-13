27d6dbf79b0bf30ef66a4e914a49189bc142ac4a
Torbjorn Granlund <tege@gmplib.org>
2009-12-13 16:31:20 +0100

    Rework buffer handling.

---
+++ tests/mpn/t-toom42.c
@@ -0,0 +1,10 @@
+#define mpn_toomMN_mul mpn_toom42_mul
+#define mpn_toomMN_mul_itch mpn_toom42_mul_itch
+
+#define MIN_AN 10
+#define MIN_BN(an) (((an) + 7) >> 2)
+#define MAX_BN(an) ((2*(an)-5) / (size_t) 3)
+
+#define COUNT 2000
+
+#include "toom-shared.h"
