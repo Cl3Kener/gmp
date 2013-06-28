4da1b1c2cde5760013e6fb93c7060fd59499059b
Marc Glisse <marc.glisse@inria.fr>
2013-06-28 21:09:37 +0200

    Use GMP_DECIMAL_POINT more consistently.

---
+++ mpn/arm/rshift.asm
@@ -0,0 +1,75 @@
+dnl  ARM mpn_rshift.
+
+dnl  Contributed to the GNU project by Torbjörn Granlund.
+
+dnl  Copyright 1997, 2000, 2001, 2012 Free Software Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 3 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
+
+include(`../config.m4')
+
+C	     cycles/limb
+C StrongARM	 ?
+C XScale	 ?
+C Cortex-A7	 ?
+C Cortex-A8	 ?
+C Cortex-A9	 3.5
+C Cortex-A15	 ?
+
+define(`rp',  `r0')
+define(`up',  `r1')
+define(`n',   `r2')
+define(`cnt', `r3')
+define(`tnc', `r12')
+
+ASM_START()
+PROLOGUE(mpn_rshift)
+	push	{r4, r6, r7, r8}
+	ldr	r4, [up]
+	rsb	tnc, cnt, #32
+
+	mov	r7, r4, lsr cnt
+	tst	n, #1
+	beq	L(evn)			C n even
+
+L(odd):	subs	n, n, #2
+	bcc	L(1)			C n = 1
+	ldr	r8, [up, #4]!
+	b	L(mid)
+
+L(evn):	ldr	r6, [up, #4]!
+	subs	n, n, #2
+	beq	L(end)
+
+L(top):	ldr	r8, [up, #4]!
+	orr	r7, r7, r6, lsl tnc
+	str	r7, [rp], #4
+	mov	r7, r6, lsr cnt
+L(mid):	ldr	r6, [up, #4]!
+	orr	r7, r7, r8, lsl tnc
+	str	r7, [rp], #4
+	mov	r7, r8, lsr cnt
+	subs	n, n, #2
+	bgt	L(top)
+
+L(end):	orr	r7, r7, r6, lsl tnc
+	str	r7, [rp], #4
+	mov	r7, r6, lsr cnt
+L(1):	str	r7, [rp]
+	mov	r0, r4, lsl tnc
+	pop	{r4, r6, r7, r8}
+	bx	r14
+EPILOGUE()
---
+++ mpn/sparc64/gcd_1.asm
@@ -0,0 +1,125 @@
+dnl  SPARC64 mpn_gcd_1.
+
+dnl  Based on the K7 gcd_1.asm, by Kevin Ryde.  Rehacked for SPARC by Torbjörn
+dnl  Granlund.
+
+dnl  Copyright 2000, 2001, 2002, 2005, 2009, 2011, 2012, 2013 Free Software
+dnl  Foundation, Inc.
+
+dnl  This file is part of the GNU MP Library.
+
+dnl  The GNU MP Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 3 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The GNU MP Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.
+
+include(`../config.m4')
+
+
+C		  cycles/bit (approx)
+C UltraSPARC 1&2:	 5.1
+C UltraSPARC 3:		 5.0
+C UltraSPARC T1:	11.4
+C UltraSPARC T3:	10
+C UltraSPARC T4:	 6
+C Numbers measured with: speed -CD -s32-64 -t32 mpn_gcd_1
+
+C ctz_table[n] is the number of trailing zeros on n, or MAXSHIFT if n==0.
+
+deflit(MAXSHIFT, 7)
+deflit(MASK, eval((m4_lshift(1,MAXSHIFT))-1))
+
+	RODATA
+	TYPE(ctz_table,object)
+ctz_table:
+	.byte	MAXSHIFT
+forloop(i,1,MASK,
+`	.byte	m4_count_trailing_zeros(i)
+')
+	SIZE(ctz_table,.-ctz_table)
+
+C Threshold of when to call bmod when U is one limb.  Should be about
+C (time_in_cycles(bmod_1,1) + call_overhead) / (cycles/bit).
+define(`BMOD_THRES_LOG2', 14)
+
+C INPUT PARAMETERS
+define(`up',    `%i0')
+define(`n',     `%i1')
+define(`v0',    `%i2')
+
+
+ASM_START()
+	REGISTER(%g2,#scratch)
+	REGISTER(%g3,#scratch)
+PROLOGUE(mpn_gcd_1)
+	save	%sp, -192, %sp
+	ldx	[up+0], %g1		C U low limb
+	mov	-1, %i4
+	or	v0, %g1, %g2		C x | y
+
+L(twos):
+	inc	%i4
+	andcc	%g2, 1, %g0
+	bz,a	%xcc, L(twos)
+	 srlx	%g2, 1, %g2
+
+L(divide_strip_y):
+	andcc	v0, 1, %g0
+	bz,a	%xcc, L(divide_strip_y)
+	 srlx	v0, 1, v0
+
+	cmp	n, 1			C if n > 1 we need
+	bnz	%xcc, L(bmod)		C to call bmod_1
+	 nop
+
+C Both U and V are single limbs, reduce with bmod if u0 >> v0.
+	srlx	%g1, BMOD_THRES_LOG2, %g2
+	cmp	%g2, v0
+	bleu	%xcc, L(noreduce)
+	 mov	%g1, %o0
+
+L(bmod):
+	mov	up, %o0
+	mov	n, %o1
+	mov	v0, %o2
+	call	mpn_modexact_1c_odd
+	 mov	0, %o3
+
+L(noreduce):
+
+	LEA64(ctz_table, i5, g4)
+
+	cmp	%o0, 0
+	bnz	%xcc, L(mid)
+	 and	%o0, MASK, %g3		C
+
+	return	%i7+8
+	 sllx	%o2, %o4, %o0		C CAUTION: v0 alias for o2
+
+	ALIGN(16)
+L(top):	movcc	%xcc, %l4, v0		C v = min(u,v)
+	movcc	%xcc, %l2, %o0		C u = |v - u]
+L(mid):	ldub	[%i5+%g3], %g5		C
+	brz,a,pn %g3, L(shift_alot)	C
+	 srlx	%o0, MAXSHIFT, %o0
+	srlx	%o0, %g5, %l4		C new u, odd
+	subcc	v0, %l4, %l2		C v - u, set flags for branch and movcc
+	sub	%l4, v0, %o0		C u - v
+	bnz,pt	%xcc, L(top)		C
+	 and	%l2, MASK, %g3		C extract low MAXSHIFT bits from (v-u)
+
+	return	%i7+8
+	 sllx	%o2, %o4, %o0		C CAUTION: v0 alias for o2
+
+L(shift_alot):
+	b	L(mid)
+	 and	%o0, MASK, %g3		C
+EPILOGUE()
